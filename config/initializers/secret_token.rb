89e0383be83db0e80d91b15e9aea368f98c5b0cb
Jorge Pardiñas <jorge.parbros@parbros.net>
2013-02-28 08:52:50 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedInnovadores::Application.config.secret_token = '68211836d5610f402fb178f9d26102cdf735471d675154b9bcad1f99537e7d0df3ab2645d9d7b973ff2f80ba03235336f9996d36db0a73c095a3288c4d0906ea'
