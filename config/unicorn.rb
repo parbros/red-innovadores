382a459b15e15372e89f31bf60735308c42a3855
Jorge Pardiñas <jorge.parbros@parbros.net>
2013-03-07 01:16:44 -0600

    arreglando configuracion de unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,7 +21,7 @@ worker_processes 3
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/home/ubuntu/app/current" # available in 0.94.0+
+working_directory "/root/app/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
