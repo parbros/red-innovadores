19ac2b0a467fd209aeb61f24b94d16ec4c990748
Jorge Pardiñas <jorge.parbros@parbros.net>
2013-03-21 07:42:30 -0600

    cambiando el nombre

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -15,7 +15,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "Red Innovadores"
+  config.site_name = "Red Innovacion"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
