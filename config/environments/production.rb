1a724d3ad0ebfb676d5f946c10c8b41b2e502916
Jorge Pardiñas <jorge.parbros@parbros.net>
2013-03-21 07:42:19 -0600

    configuracion del email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ RedInnovadores::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, 
+    :enable_starttls_auto => true,
+    :user_name => "jorge.parbros@parbros.net",
+    :password  => "HQx21FVvhOb57nIsz7O7pA",
+    :authentication => 'login'
+  }
 end
