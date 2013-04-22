2200f1778a0e43b3c91dff3dfcaa32f04282e3e1
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-04-22 17:43:40 -0500

    seteando para produccion

--- app/models/remote_user.rb
+++ app/models/remote_user.rb
@@ -3,15 +3,15 @@ require 'uri'
 
 class RemoteUser < Typhoid::Resource
 
-  # self.site = 'http://lms.redinnovacion.org'  # The base-url for where we plan to retrieve data
-  self.site = 'http://localhost:3020'  # The base-url for where we plan to retrieve data
+  self.site = 'http://lms.redinnovacion.org'  # The base-url for where we plan to retrieve data
+  # self.site = 'http://localhost:3020'  # The base-url for where we plan to retrieve data
   self.path = '/api/v1/accounts/1/users' # Specific path to get the data for this Class
   
   # development access token
-  ACCESS_TOKEN = 'NKcZzqdkrjLzvu8j7anK5yMs2og2wMCh3BDSgO72W9ZU08eqXDrFzQXOLHnhOCoR'
+  # ACCESS_TOKEN = 'NKcZzqdkrjLzvu8j7anK5yMs2og2wMCh3BDSgO72W9ZU08eqXDrFzQXOLHnhOCoR'
   
   #production access token
-  # ACCESS_TOKEN = '1XHwVBPAnk46sLXw1B8NBEzZBYD8HQAgtLMvBDCHQ6HAuqHNNWglkBb9BWqARDDW'
+  ACCESS_TOKEN = '1XHwVBPAnk46sLXw1B8NBEzZBYD8HQAgtLMvBDCHQ6HAuqHNNWglkBb9BWqARDDW'
 
   def self.create_user(pseudonym_unique_id, pseudonym_password)
     # request = build_request("#{site}#{path}?user[name]=#{username}&pseudonym[unique_id]=#{pseudonym_unique_id}&pseudonym[password]=#{pseudonym_password}&access_token=skSXWvkSeLotRT1ecatzN0oedHuRDt9d2gch3qxpBkpdvU8OvtbDTPceBNaIUOUu", {method: :post})
