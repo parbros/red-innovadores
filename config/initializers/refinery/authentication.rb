62562c2e56d7ce95fceeeda3dddfa6776b62a9fa
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-04-29 17:30:28 -0500

    settings de production

--- config/initializers/refinery/authentication.rb
+++ config/initializers/refinery/authentication.rb
@@ -1,6 +1,6 @@
 Devise.setup do |config| 
-  # config.cas_base_url = "http://auth.redinnovacion.org/"
-  config.cas_base_url = "http://localhost:8888/"
+  config.cas_base_url = "http://auth.redinnovacion.org/"
+  # config.cas_base_url = "http://localhost:8888/"
   config.cas_create_user = false
 end 
 
