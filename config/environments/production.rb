568e64bb4551f9e6b75dbf20b9bb78aa9041ac19
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-04-29 17:44:23 -0500

    settings de production host url default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ RedInnovadores::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.action_mailer.default_url_options = { :host => 'redinnovacion.org' }
+  
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587, 
