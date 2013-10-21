a224109da57e338167e5e409632959bf555d14cc
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-10-21 17:00:41 -0500

    removing rubyproof

--- config/application.rb
+++ config/application.rb
@@ -70,7 +70,5 @@ module RedInnovadores
     config.to_prepare do
       Refinery.searchable_models = [Refinery::Page, Refinery::Blog::Post, Refinery::Experiences::Experience, Refinery::Ideas::Idea]
     end
-    
-    config.middleware.use ::Rack::PerftoolsProfiler, :default_printer => 'gif', :bundler => true
   end
 end
