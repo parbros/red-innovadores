50beabc39604d56c9fbf111fbb3dbe0c6385d226
Jorge Pardiñas <jparbros@gmail.com>
2014-04-23 20:59:14 -0500

    refinery/comment message por body

--- app/models/refinery/blog/comment.rb
+++ app/models/refinery/blog/comment.rb
@@ -1,8 +1,14 @@
 module Refinery
   module Blog
     class Comment < Comment
+
+      def message
+        body
+      end
       
-      
+      def message=(new_message)
+        body = new_message
+      end
     end
   end
 end
