5a3f41e45b9e59a66269aa57d851accdc70059ff
Jorge Pardiñas <jparbros@gmail.com>
2014-04-23 22:06:40 -0500

    paginacion

--- app/models/refinery/blog/comment.rb
+++ app/models/refinery/blog/comment.rb
@@ -1,15 +1,17 @@
 module Refinery
   module Blog
     class Comment < Comment
+      
+      self.per_page = 50
 
       def message
         self.body
       end
-      
+
       def message=(new_message)
         self.body = new_message
       end
-      
+
       def post
         commentable
       end
