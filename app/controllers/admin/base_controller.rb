a05aafc445adef99f5c68324ad0a362736bcd996
Jorge Pardiñas <jparbros@gmail.com>
2014-04-24 14:39:17 -0500

    arreglando current_refinery_user

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,6 @@ class Admin::BaseController < ApplicationController
   before_filter :authenticate_admin
 
   def authenticate_admin
-    redirect_to root_url unless refinery_current_user && refinery_current_user.forem_admin?
+    redirect_to root_url unless current_refinery_user && current_refinery_user.forem_admin?
   end
 end
