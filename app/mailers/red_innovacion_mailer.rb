e3ebe31152587c550435f093fbdd1e263bef7719
Jorge Pardiñas <jparbros@gmail.com>
2014-06-11 15:51:16 -0500

    enroll notification

--- app/mailers/red_innovacion_mailer.rb
+++ app/mailers/red_innovacion_mailer.rb
@@ -2,21 +2,28 @@
 
 class RedInnovacionMailer < ActionMailer::Base
   default :from => Refinery::Setting.find_or_set("memberships_sender_address", nil)
-  
+
   def confirmation(member)
     @member = member
     mail(:to => "#{member.full_name} <#{member.email}>", :subject => "#{member.full_name} Bienvenido a Red Innovación")
   end
-  
+
   def reset_password(member)
     @member = member
     mail(:to => "#{member.full_name} <#{member.email}>", :subject => "#{member.full_name} Bienvenido a Red Innovación")
   end
-  
+
   def comment_has_respond(comment)
     @comment = comment
     @name = comment.user ? comment.user.full_name : comment.name
     @email = comment.user ? comment.user.email : comment.email
     mail(:to => "#{@name} <#{@email}>", :subject => "#{@email} Tu comentarios ha sido respondido.")
+  end
+
+  def enroll_notificaction(enroll)
+    @enroll = enroll
+    @name = enroll.user ? enroll.user.full_name : ""
+    @email = enroll.user ? enroll.user.email : ""
+    mail(:to => "#{@name} <#{@email}>", :subject => "#{@name} Haz aplicado para un curso.")
   end
 end
--- app/models/enroll.rb
+++ app/models/enroll.rb
@@ -4,7 +4,13 @@ class Enroll < ActiveRecord::Base
   belongs_to :user, class_name: 'Refinery::User'
   belongs_to :course
 
+  after_create :send_enroll_notificaction
+
   def self.by_course_id(course_id)
     where(course_id: course_id)
+  end
+
+  def send_enroll_notificaction
+    RedInnovacionMailer.enroll_notificaction(self).deliver!
   end
 end
