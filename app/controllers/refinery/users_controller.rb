d0c512e8bd2d1318f4e5e2e29b40e9d035a046a0
Jorge Pardiñas <jparbros@gmail.com>
2014-10-13 14:20:15 -0500

    edit

--- app/controllers/refinery/users_controller.rb
+++ app/controllers/refinery/users_controller.rb
@@ -11,13 +11,20 @@ module Refinery
       @user = User.new
     end
 
+    def edit
+      @user = Refinery::User.find params[:id]
+      redirect_unless_user_editable!
+
+      @selected_plugin_names = @user.plugins.collect(&:name)
+    end
+
     # This method should only be used to create the first Refinery user.
     def create
       @user = User.new(params[:user])
 
       if @user.create_first
         flash[:message] = "<h2>#{t('welcome', :scope => 'refinery.users.create', :who => @user.username).gsub(/\.$/, '')}.</h2>".html_safe
-        
+
         sign_in(@user)
         redirect_back_or_default(refinery.admin_root_path)
       else
