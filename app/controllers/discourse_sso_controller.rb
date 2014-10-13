08f8a7904d12c0134d1af2976fc1b72bf0ebd5c4
Jorge Pardiñas <jparbros@gmail.com>
2014-10-13 11:06:39 -0500

    Fizing SSO

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -2,12 +2,18 @@ class DiscourseSsoController < ApplicationController
   def sso
     secret = "redinnovacion"
     sso = SingleSignOn.parse(request.query_string, secret)
-    sso.email = current_refinery_user.email
-    sso.name = current_refinery_user.full_name
-    sso.username = current_refinery_user.username
-    sso.external_id = current_refinery_user.id
-    sso.sso_secret = secret
 
-    redirect_to sso.to_url("http://auth.redinnovacion.org/session/sso_login")
+    if sso
+      sso.email = current_refinery_user.email
+      sso.name = current_refinery_user.full_name
+      sso.username = current_refinery_user.username
+      sso.external_id = current_refinery_user.id
+      sso.sso_secret = secret
+
+      redirect_to sso.to_url("http://auth.redinnovacion.org/session/sso_login")
+    else
+      redirect_to new_refinery_user_session_url
+    end
+
   end
 end
